00d5ce5abfb1f264541c9cf287518892ee56ab21
zhenhua <zw149@duke.edu>
2020-03-09 19:21:04 -0400

    inset code

--- customization.el
+++ customization.el
@@ -33,6 +33,31 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)))
+
 (setq org-src-tab-acts-natively t)
+(setq org-src-fontify-natively t)
+
+(defun org-insert-code-block (src-code-type)
+  "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
+  (interactive
+   (let ((src-code-types
+          '("python" "R" "emacs-lisp" "C" "sh" "java" "js" "clojure" "C++" "css"
+            "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+            "octave" "oz" "plantuml" "sass" "screen" "sql" "awk" "ditaa"
+            "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+            "scheme" "sqlite")))
+     (list (ido-completing-read "Source code type: " src-code-types))))
+  (progn
+    (newline-and-indent)
+    (insert (format "#+BEGIN_SRC %s :results graphics file :exports both\n" src-code-type))
+    (newline-and-indent)
+    (insert "#+END_SRC\n")
+    (previous-line 2)))
+;;    (org-edit-src-code)))
+
 (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
+(add-hook 'org-mode-hook '(lambda ()
+                            ;; keybinding for inserting code blocks
+                            (local-set-key (kbd "C-S-i")'org-insert-code-block)))
+
 ;;end
