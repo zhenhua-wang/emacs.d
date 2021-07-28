da5fc92ff3e2664bfac7919d54128950e14dea2e
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2021-07-28 04:26:02 -0500

    delete helm related packages

--- elpa/helm-core-20210716.1933/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20210716.1933" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "3ff6e02b629ada63f9bc7e4814a39bd5f7b108eb")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
