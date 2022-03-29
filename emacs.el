a9ef491c98baf31820a0cbbc6d0884be170e02be
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-03-29 15:14:04 -0500

    remove use package for general setting

--- emacs.el
+++ emacs.el
@@ -68,77 +68,74 @@
 
 (server-start)
 
-(use-package emacs
-  :custom
-  ;; completion
-  (completion-cycle-threshold nil)
-  (tab-always-indent 'complete)
-  (completions-detailed t)
-  (completion-ignore-case t)
-  ;; Revert Dired and other buffers
-  (global-auto-revert-non-file-buffers t)
-  ;; Use spaces instead of tabs for indentation
-  (indent-tabs-mode nil)
-  ;; echo area show only 1 line of doc
-  (eldoc-echo-area-use-multiline-p nil)
-  ;; fix minibuffer size
-  (resize-mini-windows nil)
-  :init
-  ;; ------------------- simplify yes no ---------------
-  (defun yes-or-no-p->-y-or-n-p (orig-fun &rest r)
-    (cl-letf (((symbol-function 'yes-or-no-p) #'y-or-n-p))
-      (apply orig-fun r)))
-  (advice-add 'kill-buffer :around #'yes-or-no-p->-y-or-n-p)
-  ;; ------------------- modes    ---------------------
-  (global-visual-line-mode 1)
-  ;; Revert buffers when the underlying file has changed
-  (global-auto-revert-mode 1)
-  ;; hightlight current row
-  (global-hl-line-mode 1)
-  ;; ------------------- key bind ---------------------
-  ;; Keybonds
-  (global-set-key (kbd "s-z") 'undo)
-  (global-set-key (kbd "s-x") 'kill-region)
-  (global-set-key (kbd "s-c") 'kill-ring-save)
-  (global-set-key (kbd "s-v") 'yank)
-  (global-set-key (kbd "s-a") 'mark-whole-buffer)
-  (global-set-key (kbd "s-s") 'save-buffer)
-  (global-set-key (kbd "s-l") 'goto-line)
-  (global-set-key (kbd "s-q") 'kill-current-buffer)
-  ;; vterm
-  (global-set-key (kbd "s-e") 'vterm)
-  ;; eldoc
-  ;; (global-set-key (kbd "s-d") 'eldoc-doc-buffer)
-  ;; winner undo/redo
-  (global-set-key (kbd "s-u") 'winner-undo)
-  (global-set-key (kbd "s-U") 'winner-redo)
-  ;; projectile find file
-  (global-set-key (kbd "s-p") 'counsel-projectile-switch-project)
-  ;; Make ESC quit prompts
-  ;; (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-  (global-set-key (kbd "<escape>") (kbd "C-g"))
-  ;; window operations
-  (global-set-key (kbd "s-w") 'delete-window)
-  (global-set-key (kbd "s-t") 'split-window-sensibly-prefer-horizontal)
-  (global-set-key [s-left] 'windmove-left)          ; move to left window
-  (global-set-key [s-right] 'windmove-right)        ; move to right window
-  (global-set-key [s-up] 'windmove-up)              ; move to upper window
-  (global-set-key [s-down] 'windmove-down)          ; move to lower window
-  ;; check dict
-  (global-set-key (kbd "C-c w") 'wordnut-search)
-  (global-set-key (kbd "C-c W") 'wordnut-lookup-current-word)
-  ;; toggle transparency
-  (global-set-key (kbd "C-c t") 'zw/toggle-transparency)
-  ;; get passwed
-  ;; (global-set-key (kbd "C-c p") 'zw/get-passwd)
-  ;; toggle input
-  (global-set-key (kbd "C-\\") 'toggle-input-method)
-  ;; consistent with EXWM
-  (pcase system-type
-    ('darwin
-     (progn
-       (setq mac-command-modifier 'super)
-       (setq mac-option-modifier 'meta)))))
+;; completion
+(setq completion-cycle-threshold nil)
+(setq tab-always-indent 'complete)
+(setq completions-detailed t)
+(setq completion-ignore-case t)
+;; Revert Dired and other buffers
+(setq global-auto-revert-non-file-buffers t)
+;; Use spaces instead of tabs for indentation
+(setq indent-tabs-mode nil)
+;; echo area show only 1 line of doc
+(setq eldoc-echo-area-use-multiline-p nil)
+;; fix minibuffer size
+(setq resize-mini-windows nil)
+;; ------------------- simplify yes no ---------------
+(defun yes-or-no-p->-y-or-n-p (orig-fun &rest r)
+  (cl-letf (((symbol-function 'yes-or-no-p) #'y-or-n-p))
+    (apply orig-fun r)))
+(advice-add 'kill-buffer :around #'yes-or-no-p->-y-or-n-p)
+;; ------------------- modes    ---------------------
+(global-visual-line-mode 1)
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
+;; hightlight current row
+(global-hl-line-mode 1)
+;; ------------------- key bind ---------------------
+;; Keybonds
+(global-set-key (kbd "s-z") 'undo)
+(global-set-key (kbd "s-x") 'kill-region)
+(global-set-key (kbd "s-c") 'kill-ring-save)
+(global-set-key (kbd "s-v") 'yank)
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "s-l") 'goto-line)
+(global-set-key (kbd "s-q") 'kill-current-buffer)
+;; vterm
+(global-set-key (kbd "s-e") 'vterm)
+;; eldoc
+;; (global-set-key (kbd "s-d") 'eldoc-doc-buffer)
+;; winner undo/redo
+(global-set-key (kbd "s-u") 'winner-undo)
+(global-set-key (kbd "s-U") 'winner-redo)
+;; projectile find file
+(global-set-key (kbd "s-p") 'counsel-projectile-switch-project)
+;; Make ESC quit prompts
+;; (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "<escape>") (kbd "C-g"))
+;; window operations
+(global-set-key (kbd "s-w") 'delete-window)
+(global-set-key (kbd "s-t") 'split-window-sensibly-prefer-horizontal)
+(global-set-key [s-left] 'windmove-left)          ; move to left window
+(global-set-key [s-right] 'windmove-right)        ; move to right window
+(global-set-key [s-up] 'windmove-up)              ; move to upper window
+(global-set-key [s-down] 'windmove-down)          ; move to lower window
+;; check dict
+(global-set-key (kbd "C-c w") 'wordnut-search)
+(global-set-key (kbd "C-c W") 'wordnut-lookup-current-word)
+;; toggle transparency
+(global-set-key (kbd "C-c t") 'zw/toggle-transparency)
+;; get passwed
+;; (global-set-key (kbd "C-c p") 'zw/get-passwd)
+;; toggle input
+(global-set-key (kbd "C-\\") 'toggle-input-method)
+;; consistent with EXWM
+(pcase system-type
+  ('darwin
+   (progn
+     (setq mac-command-modifier 'super)
+     (setq mac-option-modifier 'meta))))
 
 ;; (use-package spacegray-theme)
 ;; (use-package doom-themes)
@@ -243,36 +240,34 @@
      (progn
        (display-battery-mode)))))
 
-(use-package emacs
-  :config
-  (tab-bar-mode 1)
-  (setq tab-bar-tab-name-function 'tab-bar-tab-name-truncated)
-  (setq tab-bar-new-tab-choice "*scratch*")
-  (setq tab-bar-close-button
-        (propertize "  "
-                    'close-tab t
-                    :help "Click to close tab"))
-  (setq tab-bar-new-button 
-        (propertize "  "
-                    'new-tab t
-                    :help "Click to create tab"))
-  (global-set-key (kbd "s-1") (lambda () (interactive) (tab-select 1)))
-  (global-set-key (kbd "s-2") (lambda () (interactive) (tab-select 2)))
-  (global-set-key (kbd "s-3") (lambda () (interactive) (tab-select 3)))
-  (global-set-key (kbd "s-4") (lambda () (interactive) (tab-select 4)))
-  (global-set-key (kbd "s-5") (lambda () (interactive) (tab-select 5)))
-  (global-set-key (kbd "s-n") 'tab-new)
-  ;; (global-set-key (kbd "s-d") 'tab-close)
-  (set-face-attribute 'tab-bar-tab nil
-                      ;; :background (face-background 'mode-line)
-                      :background (face-background 'default)
-                      :underline "#950b96"
-                      :font zw/default-font)
-  (set-face-attribute 'tab-bar-tab-inactive nil
-                      :background (face-background 'default)
-                      :underline nil
-                      :font zw/default-font)
-  (set-face-background 'tab-bar (face-background 'default)))
+(tab-bar-mode 1)
+(setq tab-bar-tab-name-function 'tab-bar-tab-name-truncated)
+(setq tab-bar-new-tab-choice "*scratch*")
+(setq tab-bar-close-button
+      (propertize "  "
+                  'close-tab t
+                  :help "Click to close tab"))
+(setq tab-bar-new-button 
+      (propertize "  "
+                  'new-tab t
+                  :help "Click to create tab"))
+(global-set-key (kbd "s-1") (lambda () (interactive) (tab-select 1)))
+(global-set-key (kbd "s-2") (lambda () (interactive) (tab-select 2)))
+(global-set-key (kbd "s-3") (lambda () (interactive) (tab-select 3)))
+(global-set-key (kbd "s-4") (lambda () (interactive) (tab-select 4)))
+(global-set-key (kbd "s-5") (lambda () (interactive) (tab-select 5)))
+(global-set-key (kbd "s-n") 'tab-new)
+;; (global-set-key (kbd "s-d") 'tab-close)
+(set-face-attribute 'tab-bar-tab nil
+                    ;; :background (face-background 'mode-line)
+                    :background (face-background 'default)
+                    :underline "#950b96"
+                    :font zw/default-font)
+(set-face-attribute 'tab-bar-tab-inactive nil
+                    :background (face-background 'default)
+                    :underline nil
+                    :font zw/default-font)
+(set-face-background 'tab-bar (face-background 'default))
 
 ;; line number mode
 (column-number-mode)
