90fb5aa8a3c526cf3b20d892a094143585d0509b
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-06-16 23:28:11 -0500

    cleanup

--- early-init.el
+++ early-init.el
@@ -12,7 +12,7 @@
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
-(menu-bar-mode -1)            ; Disable the menu bar
+(menu-bar-mode -1)          ; Disable the menu bar
 
 ;; Set frame
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
