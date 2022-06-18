96705d905c9f424d949f1123b8d2743474be939c
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-06-18 18:25:08 -0500

    cleanup corfu

--- lisp/zw-corfu.el
+++ lisp/zw-corfu.el
@@ -46,52 +46,52 @@
   (defun zw/lsp-mode-setup-completion ()
     (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
           '(orderless))
-    (setq lsp-completion-mode :none)))
-
-(use-package dabbrev
-  :custom
-  ;; since cape-dabbrev cannot replace case, I will set it to nil for now.
-  (dabbrev-case-fold-search nil)
-  (dabbrev-case-replace t))
-
-(use-package kind-icon
-  :after corfu
-  :init
-  (setq kind-icon-use-icons t
-	kind-icon-default-face 'corfu-default)
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-(use-package corfu-doc
-  :hook
-  (corfu-mode . corfu-doc-mode)
-  :bind
-  (:map corfu-map
-        ("M-p" . corfu-doc-scroll-down)
-        ("M-n" . corfu-doc-scroll-up)))
-
-
-;; orderless
-(use-package orderless
-  ;; :after corfu
+    (setq lsp-completion-mode :none))
   :config
-  (setq completion-styles '(orderless partial-completion basic)
-        completion-category-defaults nil
-        completion-category-overrides nil))
+  (use-package dabbrev
+    :custom
+    ;; since cape-dabbrev cannot replace case, I will set it to nil for now.
+    (dabbrev-case-fold-search nil)
+    (dabbrev-case-replace t))
 
-;; Add extensions
-(use-package cape
-  :after corfu
-  :bind
-  ("C-c f" . cape-file)
-  ("C-c d" . cape-dabbrev)
-  :hook
-  (after-change-major-mode . add-cape-completion)
-  :init
-  (setq cape-dabbrev-min-length 1)
-  (defun add-cape-completion ()
-    ;; Add `completion-at-point-functions', used by `completion-at-point'.
-    (add-to-list 'completion-at-point-functions #'cape-file)
-    (add-to-list 'completion-at-point-functions #'cape-dabbrev t)))
+  (use-package kind-icon
+    :after corfu
+    :init
+    (setq kind-icon-use-icons t
+	  kind-icon-default-face 'corfu-default)
+    (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+  (use-package corfu-doc
+    :hook
+    (corfu-mode . corfu-doc-mode)
+    :bind
+    (:map corfu-map
+          ("M-p" . corfu-doc-scroll-down)
+          ("M-n" . corfu-doc-scroll-up)))
+
+
+  ;; orderless
+  (use-package orderless
+    ;; :after corfu
+    :config
+    (setq completion-styles '(orderless partial-completion basic)
+          completion-category-defaults nil
+          completion-category-overrides nil))
+
+  ;; Add extensions
+  (use-package cape
+    :after corfu
+    :bind
+    ("C-c f" . cape-file)
+    ("C-c d" . cape-dabbrev)
+    :hook
+    (after-change-major-mode . add-cape-completion)
+    :init
+    (setq cape-dabbrev-min-length 1)
+    (defun add-cape-completion ()
+      ;; Add `completion-at-point-functions', used by `completion-at-point'.
+      (add-to-list 'completion-at-point-functions #'cape-file)
+      (add-to-list 'completion-at-point-functions #'cape-dabbrev t))))
 
 (provide 'zw-corfu)
 
