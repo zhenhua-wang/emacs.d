9468f82a8ec66b2e5c0a5576d5cdf83d313b7623
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-06-23 22:32:42 -0500

    org theme

--- lisp/zw-org-theme.el
+++ lisp/zw-org-theme.el
@@ -19,16 +19,16 @@
      `(org-level-7 ((t (,@headline ,@variable-tuple))))
      `(org-level-6 ((t (,@headline ,@variable-tuple))))
      `(org-level-5 ((t (,@headline ,@variable-tuple))))
-     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1 :italic t :foreground "#FF8C94" :slant italic))))
-     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25 :foreground "#D08770"))))
-     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.50 :foreground "#88C0D0"))))
-     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75 :foreground "#5E81AC"))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1 :italic t :slant italic))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25 :foreground "#a3be8c"))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.50 :foreground "#d08770"))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75 :foreground "#5e81ac"))))
      `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline t)))))))
 
 (custom-theme-set-faces
  'user
  '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-code ((t (:inherit (shadow fixed-pitch) :foreground "#242F9B" :background "#E8F9FD"))))
  '(org-document-info ((t (:foreground "dark orange"))))
  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
  '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
@@ -38,11 +38,23 @@
  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
  '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(org-latex-and-related ((t (:inherit (shadow fixed-pitch))))) ;;  :foreground "#EBCB8B"
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch) :foreground "#B25068" :background "#FFEDDB"))))
+ '(org-latex-and-related ((t (:inherit (shadow fixed-pitch))))))
  ;; hight code blocks
- '(org-block-begin-line ((t (:extend t :bold t :italic t :underline t :overline nil))))
- '(org-block-end-line ((t (:extend t :bold t :italic t :underline nil :overline nil)))))
+(set-face-attribute 'org-block-begin-line nil
+		    :foreground "#4c566a"
+		    :bold t
+		    :italic t
+		    :underline t
+		    :overline nil
+		    :extend t)
+(set-face-attribute 'org-block-end-line nil
+		    :background	(face-background 'org-block)
+		    :bold t
+		    :italic t
+		    :underline nil
+		    :overline nil
+		    :extend t)
 
 (provide 'zw-org-theme)
 ;;; zw-org-theme.el ends here
