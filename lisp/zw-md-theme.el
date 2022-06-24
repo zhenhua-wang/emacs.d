1e87246a8fedd5153301d2078ae051bfe3e0b831
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-06-24 16:05:55 -0500

    md theme

--- lisp/zw-md-theme.el
+++ lisp/zw-md-theme.el
@@ -16,8 +16,8 @@
      `(markdown-header-face-3 ((t (,@headline ,@variable-tuple :height 1.25 :foreground "#D08770"))))
      `(markdown-header-face-2 ((t (,@headline ,@variable-tuple :height 1.5 :foreground "#88C0D0"))))
      `(markdown-header-face-1 ((t (,@headline ,@variable-tuple :height 1.75 :foreground "#5E81AC"))))
-     `(markdown-metadata-value-face ((t (:inherit markdown-metadata-value-face :height 1.4 :underline t :weight bold :foreground ,base-font-color))))
-     `(markdown-metadata-key-face ((t (:inherit markdown-metadata-key-face :height 1.2)))))))
+     `(markdown-metadata-value-face ((t (,@variable-tuple :inherit markdown-metadata-value-face :height 1.7 :underline t :weight bold :foreground ,base-font-color))))
+     `(markdown-metadata-key-face ((t (:inherit (thin markdown-metadata-key-face) :height 0.8)))))))
 
 (provide 'zw-md-theme)
 ;;; zw-md-theme.el ends here
