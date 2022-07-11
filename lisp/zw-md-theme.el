46e50c49cfb2e0c5b2f776b399b41f2f3e2d7b2f
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-07-11 00:36:00 -0500

    markdown theme

--- lisp/zw-md-theme.el
+++ lisp/zw-md-theme.el
@@ -14,7 +14,7 @@
      `(markdown-header-face-5 ((t (,@headline ,@variable-tuple))))
      `(markdown-header-face-4 ((t (,@headline ,@variable-tuple :foreground "#FF8C94"))))
      `(markdown-header-face-3 ((t (,@headline ,@variable-tuple :height 1.25 :foreground "#D08770"))))
-     `(markdown-header-face-2 ((t (,@headline ,@variable-tuple :height 1.5 :foreground "#88C0D0"))))
+     `(markdown-header-face-2 ((t (,@headline ,@variable-tuple :height 1.5 :foreground "#446A46"))))
      `(markdown-header-face-1 ((t (,@headline ,@variable-tuple :height 1.75 :foreground "#5E81AC"))))
      `(markdown-metadata-value-face ((t (,@variable-tuple :inherit markdown-metadata-value-face :height 1.7 :underline t :weight bold :foreground ,base-font-color))))
      `(markdown-metadata-key-face ((t (:inherit (thin markdown-metadata-key-face) :height 0.8)))))))
