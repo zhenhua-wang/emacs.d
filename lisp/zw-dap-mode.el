77d9e2f5a0e20c5fba918e482923173b37829c30
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-04 15:19:12 -0500

    update dap-mode

--- lisp/zw-dap-mode.el
+++ lisp/zw-dap-mode.el
@@ -5,13 +5,10 @@
 (use-package dap-mode
   :hook
   (after-init . dap-auto-configure-mode)
-  (dap-stopped . (lambda (arg) (debug-hydra)))
-  (dap-terminated . (lambda (args) (debug-hydra/nil)))
+  (dap-stopped . (lambda (arg) (dap-hydra)))
   (python-mode . (lambda () (require 'dap-python)))
   ((c-mode c++-mode objc-mode swift-mode) . (lambda ()
 					      (require 'dap-cpptools)
-					      (dap-cpptools-setup)))
-  :init
-  (setq dap-auto-configure-features '(sessions locals breakpoints expressions controls)))
+					      (dap-cpptools-setup))))
 
 (provide 'zw-dap-mode)
