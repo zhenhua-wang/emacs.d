9719fb37ffe8306d6733b68cafb6d94a0cad35fd
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-07 14:19:46 -0500

    rm eaf keys

--- lisp/zw-eaf.el
+++ lisp/zw-eaf.el
@@ -4,9 +4,5 @@
 (require 'eaf-pdf-viewer)
 (eaf-setq eaf-pdf-default-zoom  2)
 (eaf-setq eaf-pdf-dark-mode "ignore")
-(bind-keys :map eaf-mode-map
-           ("p" . eaf-py-proxy-scroll_up_page)
-           ("n" . eaf-py-proxy-scroll_down_page)
-           ("+" . eaf-py-proxy-zoom_in))
 
 (provide 'zw-eaf)
