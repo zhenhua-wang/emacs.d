ce15f5484f695f44cef273d2474e5e86e26130d3
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-22 21:01:00 -0500

    use eaf on linux

--- lisp/zw-eaf.el
+++ lisp/zw-eaf.el
@@ -1,8 +1,11 @@
 ;; eaf
 (add-to-list 'load-path "~/Workspace/emacs-application-framework/")
 (require 'eaf)
+(require 'eaf-browser)
 (require 'eaf-pdf-viewer)
 (eaf-setq eaf-pdf-default-zoom  2)
 (eaf-setq eaf-pdf-dark-mode "ignore")
+(eaf-setq eaf-webengine-default-zoom  "2")
+(setq browse-url-browser-function 'eaf-open-browser)
 
 (provide 'zw-eaf)
