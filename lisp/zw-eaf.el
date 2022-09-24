4ca44fbe1710d427f1c08b05aecdcb53c0834c3c
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-24 14:40:21 -0500

    disable eaf browser dark mode

--- lisp/zw-eaf.el
+++ lisp/zw-eaf.el
@@ -5,8 +5,9 @@
 (require 'eaf-pdf-viewer)
 (eaf-setq eaf-pdf-default-zoom  2)
 (eaf-setq eaf-pdf-dark-mode "ignore")
-(eaf-setq eaf-webengine-default-zoom  "2")
-(eaf-setq eaf-browser-enable-adblocker  t)
+(eaf-setq eaf-webengine-default-zoom "2")
+(eaf-setq eaf-browser-enable-adblocker t)
+(eaf-setq eaf-browser-dark-mode nil)
 (setq browse-url-browser-function 'eaf-open-browser)
 
 (provide 'zw-eaf)
