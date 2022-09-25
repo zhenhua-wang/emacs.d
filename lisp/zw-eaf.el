aa100c2717ba4c83f756cf84e7d0e4f3b827fbe8
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-25 02:35:20 -0500

    use straight install eaf

--- lisp/zw-eaf.el
+++ lisp/zw-eaf.el
@@ -1,14 +1,18 @@
 ;; eaf
-(add-to-list 'load-path "~/Workspace/emacs-application-framework/")
-(require 'eaf)
-(require 'eaf-browser)
-(require 'eaf-pdf-viewer)
-(eaf-setq eaf-pdf-default-zoom  2)
-(eaf-setq eaf-pdf-dark-mode "ignore")
-(eaf-setq eaf-webengine-default-zoom "2")
-(eaf-setq eaf-browser-enable-adblocker t)
-(eaf-setq eaf-browser-dark-mode nil)
-(setq browse-url-browser-function 'eaf-open-browser)
-(bind-keys ("s-o" . eaf-open-this-buffer))
+(use-package eaf
+  :straight '(eaf :host github :repo "emacs-eaf/emacs-application-framework"
+                  :files ("*"))
+  :config
+  ;; browser
+  (require 'eaf-browser)
+  (eaf-setq eaf-webengine-default-zoom "2")
+  (eaf-setq eaf-browser-enable-adblocker t)
+  (eaf-setq eaf-browser-dark-mode nil)
+  (setq browse-url-browser-function 'eaf-open-browser)
+  ;; pdf
+  (require 'eaf-pdf-viewer)
+  (eaf-setq eaf-pdf-default-zoom  2)
+  (eaf-setq eaf-pdf-dark-mode "ignore")
+  (bind-keys ("s-o" . eaf-open-this-buffer)))
 
 (provide 'zw-eaf)
