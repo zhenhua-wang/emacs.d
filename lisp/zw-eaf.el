65e6354dd2a733a4da35b68a52e89ebe23f73f27
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-25 23:42:12 -0500

    add keybindings in eaf

--- lisp/zw-eaf.el
+++ lisp/zw-eaf.el
@@ -14,6 +14,8 @@
     (eaf-setq eaf-browser-enable-adblocker t)
     (eaf-setq eaf-browser-dark-mode nil)
     (setq browse-url-browser-function 'eaf-open-browser)
+    (eaf-bind-key scroll_to_begin "M-<" eaf-browser-keybinding)
+    (eaf-bind-key scroll_to_bottom "M->" eaf-browser-keybinding)
     (eaf-bind-key insert_or_scroll_up_page "n" eaf-browser-keybinding)
     (eaf-bind-key insert_or_scroll_down_page "p" eaf-browser-keybinding)
     (eaf-bind-key kill_text "s-x" eaf-browser-keybinding)
@@ -21,15 +23,19 @@
     (eaf-bind-key yank_text "s-v" eaf-browser-keybinding)
     (eaf-bind-key undo_action "s-z" eaf-browser-keybinding)
     (eaf-bind-key redo_action "s-Z" eaf-browser-keybinding)
-    (eaf-bind-key select_all_or_input_text "s-a" eaf-browser-keybinding))
+    (eaf-bind-key select_all_or_input_text "s-a" eaf-browser-keybinding)
+    (eaf-bind-key search_text_forward "s-f" eaf-browser-keybinding))
   ;; pdf
   (when zw/enable-eaf-pdf-p
     (require 'eaf-pdf-viewer)
     (eaf-setq eaf-pdf-default-zoom  2)
     (eaf-setq eaf-pdf-dark-mode "ignore")
+    (eaf-bind-key scroll_to_begin "M-<" eaf-pdf-viewer-keybinding)
+    (eaf-bind-key scroll_to_end "M->" eaf-pdf-viewer-keybinding)
     (eaf-bind-key scroll_up_page "n" eaf-pdf-viewer-keybinding)
     (eaf-bind-key scroll_down_page "p" eaf-pdf-viewer-keybinding)
-    (eaf-bind-key copy_select "s-c" eaf-pdf-viewer-keybinding)))
+    (eaf-bind-key copy_select "s-c" eaf-pdf-viewer-keybinding)
+    (eaf-bind-key search_text_forward "s-f" eaf-pdf-viewer-keybinding)))
 
 (defun zw/eaf-open-this-buffer ()
   "Try to open the current buffer using EAF and close old buffer"
