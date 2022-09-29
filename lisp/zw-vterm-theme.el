39d4a3cf0292865c8cb9517f71c85ce6f2b89515
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-09-28 20:03:55 -0500

    update vterm color

--- lisp/zw-vterm-theme.el
+++ lisp/zw-vterm-theme.el
@@ -7,7 +7,7 @@
   (set-face-attribute 'vterm-color-red nil :foreground "#B25068" :background "#B25068")
   (set-face-attribute 'vterm-color-green nil :foreground "#2EB086" :background "#2EB086")
   (set-face-attribute 'vterm-color-yellow nil :foreground "#F4E06D" :background "#F4E06D")
-  (set-face-attribute 'vterm-color-blue nil :foreground "#242F9B" :background "#242F9B")
+  (set-face-attribute 'vterm-color-blue nil :foreground "#47B5FF" :background "#47B5FF")
   (set-face-attribute 'vterm-color-magenta nil :foreground "#FF869E" :background "#FF869E")
   (set-face-attribute 'vterm-color-cyan nil :foreground "#4CACBC" :background "#4CACBC")
   (set-face-attribute 'vterm-color-white nil :foreground "#E5E9F0" :background "#E5E9F0"))
