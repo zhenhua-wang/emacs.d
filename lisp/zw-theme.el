c602a20cef184ef1b2d0b4b80447e71f2b4d5123
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-10-01 18:38:13 -0500

    cleanup theme

---
+++ lisp/zw-theme.el
@@ -0,0 +1,61 @@
+;; -*- lexical-binding: t -*-
+
+(let* (
+       (variable-tuple (list ':font zw/font-title))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces
+   'user
+   ;; org
+   `(org-level-8 ((t (,@headline ,@variable-tuple :weight SemiBold))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple :weight SemiBold))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple :weight SemiBold))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple :weight SemiBold))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :weight SemiBold :height 1.25 :foreground "#FF7777"))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :weight SemiBold :height 1.25 :foreground "#d08770"))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :weight SemiBold :height 1.25 :foreground "#446A46"))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :weight SemiBold :height 1.5 :foreground "#5e81ac"))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 1.625 :underline t))))
+   ;; setup fixed pitch fonts
+   `(org-ellipsis ((t (:inherit fixed-pitch))))
+   `(org-meta-line ((t (:inherit (bold fixed-pitch)))))
+   `(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+   `(org-formula ((t (:inherit fixed-pitch))))
+   `(org-latex-and-related ((t (:inherit (shadow fixed-pitch)))))
+   `(org-link ((t (:inherit fixed-pitch :foreground "royal blue" :underline t))))
+   `(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   `(org-checkbox ((t (:inherit 'fixed-pitch))))
+   `(org-property-value ((t (:inherit fixed-pitch))))
+   `(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+   `(org-document-info ((t (:foreground "dark orange"))))
+   `(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+   `(org-block ((t (:inherit fixed-pitch))))
+   `(org-block-begin-line ((t (:foreground "#4c566a" :bold t :italic t :underline t :extend t))))
+   `(org-block-end-line ((t (:background ,(face-background 'org-block) :bold t :italic t :extend t))))
+   `(org-code ((t (:inherit (shadow fixed-pitch) :background ,(face-background 'org-block) :box ,(face-foreground 'default)))))
+   `(org-verbatim ((t (:inherit (shadow fixed-pitch) :background ,(face-background 'org-block) :box ,(face-foreground 'default)))))
+
+   ;; markdown
+   `(markdown-header-face-6 ((t (,@headline ,@variable-tuple))))
+   `(markdown-header-face-5 ((t (,@headline ,@variable-tuple))))
+   `(markdown-header-face-4 ((t (,@headline ,@variable-tuple :foreground ,(face-foreground 'default)))))
+   `(markdown-header-face-3 ((t (,@headline ,@variable-tuple :height 1.25 :foreground ,(face-foreground 'default)))))
+   `(markdown-header-face-2 ((t (,@headline ,@variable-tuple :height 1.5 :foreground ,(face-foreground 'default)))))
+   `(markdown-header-face-1 ((t (,@headline ,@variable-tuple :height 1.75 :foreground ,(face-foreground 'default)))))
+   `(markdown-metadata-value-face ((t (,@variable-tuple :inherit markdown-metadata-value-face :height 1.7 :underline t :weight bold :foreground ,base-font-color))))
+   `(markdown-metadata-key-face ((t (:inherit (thin markdown-metadata-key-face) :height 0.8 :foreground "dark orange"))))
+   `(markdown-header-delimiter-face ((t (:inherit (font-lock-comment-face fixed-pitch) :height 0.8))))
+   `(markdown-language-info-face ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   `(markdown-code-face ((t (:inherit fixed-pitch :extend t))))
+   ;; vterm
+   `(vterm-color-black ((t (:foreground "#3B4252" :background "#3B4252"))))
+   `(vterm-color-red ((t (:foreground "#B25068" :background "#B25068"))))
+   `(vterm-color-green ((t (:foreground "#2EB086" :background "#2EB086"))))
+   `(vterm-color-yellow ((t (:foreground "#F4E06D" :background "#F4E06D"))))
+   `(vterm-color-blue ((t (:foreground "#47B5FF" :background "#47B5FF"))))
+   `(vterm-color-magenta ((t (:foreground "#FF869E" :background "#FF869E"))))
+   `(vterm-color-cyan ((t (:foreground "#4CACBC" :background "#4CACBC"))))
+   `(vterm-color-white ((t (:foreground "#E5E9F0" :background "#E5E9F0"))))))
+
+(provide 'zw-theme)
