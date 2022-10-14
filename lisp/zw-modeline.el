37594ef99de868888ff1df883ed5c708f8b025a4
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-10-14 00:56:30 -0500

    cleanup modeline setting

--- lisp/zw-modeline.el
+++ lisp/zw-modeline.el
@@ -333,8 +333,6 @@
   " "))
 
 ;;; misc config
-;; 3d effects
-(set-face-attribute 'mode-line nil :box '(:line-width 1 :style released-button))
 ;; repl
 (dolist (mode '(inferior-ess-mode-hook
                 inferior-python-mode-hook))
