28d265387b7940bc1a9655d954b940e049130aa9
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2022-12-29 08:58:21 -0600

    update

--- lisp/zw-theme.el
+++ lisp/zw-theme.el
@@ -117,6 +117,9 @@
      ;; diredfl
      `(diredfl-dir-name ((t (:bold t))))
 
+     ;; outline minor
+     `(outline-minor-1 ((t (:inherit (outline-minor-0 outline-1) :overline t))))
+
      ;; company-mode
      `(company-tooltip ((t (:inherit tooltip ,@fixed-font :height 150))))
      `(company-tooltip-selection ((t (:weight bold))))
