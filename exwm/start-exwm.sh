b7c0d4749afb253109389ee67f9410fb11f7fa8e
Zhenhua <wang_zhenhua1996@icloud.com>
2023-03-01 17:08:59 -0600

    update

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -20,5 +20,5 @@ export EDITOR="$VISUAL"
 export _JAVA_AWT_WM_NONREPARENTING=1
 
 # Fire it up
-# exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.emacs.d/zw-exwm.el
-exec dbus-launch emacs -mm --debug-init -l ~/.emacs.d/lisp/zw-exwm.el
+exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.emacs.d/lisp/zw-exwm.el
+# exec dbus-launch emacs -mm --debug-init -l ~/.emacs.d/lisp/zw-exwm.el
