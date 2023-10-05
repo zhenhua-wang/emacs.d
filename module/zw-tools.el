9ce51ef378b5fea8d3949cf12160c852213c3632
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2023-10-05 15:11:27 -0500

    update dired-side-bar

--- module/zw-tools.el
+++ module/zw-tools.el
@@ -66,8 +66,9 @@
   (dired-mode . dired-omit-mode)
   (dired-mode . (lambda () (visual-line-mode 0)))
   :bind ((:map global-map
-               ("s-b" . zw/dired-sidebar-toggle)
-          :map dired-mode-map
+               ("s-b" . zw/dired-sidebar-toggle))
+
+         (:map dired-mode-map
                ("<tab>" . dired-subtree-toggle)
                ("q" . zw/kill-bufer-quit-window)))
   :init
@@ -94,64 +95,86 @@
   :commands (dired-subtree-toggle))
 
 ;; ** dired side bar
-(defun zw/dired-sidebar-enable ()
-  (interactive)
-  (if (and (eq major-mode 'dired-mode)
-           (zw/side-window-p))
-      (zw-dired-sidebar-mode 1)))
+(defun zw/dired-sidebar--modeline-major-mode ()
+  "Sidebar modeline major mode."
+  (propertize "Sidebar " 'face (zw/modeline-set-face 'zw/modeline-major-mode-active
+                                                     'zw/modeline-default-inactive)))
 
-(defun zw/dired-sidebar-disable ()
+(defun zw/dired-sidebar--modeline-directory (dir)
+  "Sidebar modeline directory."
+  (car (last (split-string dir "/") 2)))
+
+(defun zw/dired-sidebar--modeline-format (sidebar-name)
+  (list "%e" " "
+        `(:eval (propertize ,(concat sidebar-name zw/modeline-separator)
+                            'face (zw/modeline-set-face 'zw/modeline-major-mode-active
+                                                        'zw/modeline-default-inactive)))
+        '(:eval (zw/modeline-remote))
+        '(:eval (zw/modeline-middle-space (zw/dired-sidebar--modeline-major-mode)))))
+
+(defun zw/dired-siderbar-display (buffer)
+  ;; bury current dired buffer when it has the same root as sidebar
+  (when (eq (current-buffer) buffer)
+    (bury-buffer))
+  ;; display sidebar
+  (display-buffer-in-side-window
+   buffer `((side . left) (slot . 0)
+            (window-width . 0.2)
+            (preserve-size . (t . nil))))
+  (select-window (get-buffer-window buffer)))
+
+(defun zw/dired-sidebar-enable (buffer)
   (interactive)
-  (zw-dired-sidebar-mode 0))
+  (with-current-buffer buffer
+    (when (eq major-mode 'dired-mode)
+      (zw-dired-sidebar-mode 1)
+      (let* ((dir (abbreviate-file-name (dired-current-directory)))
+             (current-dir (zw/dired-sidebar--modeline-directory dir))
+             (name (concat " :" dir)))
+        (dired-hide-details-mode t)
+        (rename-buffer name)
+        (setq-local mode-line-format (zw/dired-sidebar--modeline-format current-dir))
+        (zw/dired-siderbar-display buffer)
+        (set-window-dedicated-p (get-buffer-window buffer) t)))))
+
+(defun zw/dired-sidebar-disable (buffer)
+  (interactive)
+  (with-current-buffer buffer
+    (when zw-dired-sidebar-mode
+      (zw-dired-sidebar-mode 0)
+      (let* ((dir (abbreviate-file-name (dired-current-directory))))
+        (dired-hide-details-mode 0)
+        (rename-buffer dir)
+        (setq-local mode-line-format (default-value 'mode-line-format))
+        ;; close sidebar
+        (quit-window) (display-buffer buffer)
+        (set-window-dedicated-p (get-buffer-window buffer) nil)))))
 
 (defun zw/dired-sidebar-toggle ()
   "Toggle dired on left side."
   (interactive)
-  ;; close all old sidebars
-  (dolist (buffer (buffer-list))
-    (with-current-buffer buffer
-      (when zw-dired-sidebar-mode
-        (kill-buffer buffer))))
   ;; open current directory in sidebar
   (let* ((dir (abbreviate-file-name
                (or (vc-root-dir)
                    (ignore-errors (file-name-directory (buffer-file-name)))
                    default-directory)))
          (buffer (dired-noselect dir)))
-    ;; bury current dired buffer when it has the same root as sidebar
-    (when (eq (current-buffer) buffer)
-      (bury-buffer))
-    (display-buffer-in-side-window
-     buffer `((side . left) (slot . 0)
-              (window-width . 0.2)
-              (preserve-size . (t . nil))
-              (window-parameters . ((dedicated . t)))))
-    (with-current-buffer buffer (zw/dired-sidebar-enable))
-    (select-window (get-buffer-window buffer))))
+    (zw/dired-sidebar-enable buffer)))
 
 (defun zw/dired-sidebar-find-file ()
   (interactive)
   (dired-find-file)
-  (zw/dired-sidebar-enable))
+  (zw/dired-sidebar-enable (current-buffer)))
 
 (defun zw/dired-sidebar-up-directory ()
   (interactive)
   (dired-up-directory)
-  (zw/dired-sidebar-enable))
+  (zw/dired-sidebar-enable (current-buffer)))
 
 (defun zw/dired-sidebar-maximize ()
   (interactive)
   (zw/maximize-window)
-  (zw/dired-sidebar-disable))
-
-(defun zw/dired-sidebar-modeline-major-mode ()
-  "Sidebar modeline major mode."
-  (propertize "Sidebar " 'face (zw/modeline-set-face 'zw/modeline-major-mode-active
-                                                     'zw/modeline-default-inactive)))
-
-(defun zw/dired-sidebar-modeline-directory (dir)
-  "Sidebar modeline directory."
-  (car (last (split-string dir "/") 2)))
+  (zw/dired-sidebar-disable (current-buffer)))
 
 (define-minor-mode zw-dired-sidebar-mode
   "Toggle zw-dired-sidebar mode."
@@ -163,28 +186,7 @@
     (,(kbd "^") . zw/dired-sidebar-up-directory)
     (,(kbd "RET") . zw/dired-sidebar-find-file)
     (,(kbd "<mouse-2>") . zw/dired-sidebar-find-file)
-    (,(kbd "C-x 1") . zw/dired-sidebar-maximize))
-  (let* ((dir (abbreviate-file-name (dired-current-directory)))
-         (current-dir (zw/dired-sidebar-modeline-directory dir))
-         (name (concat " :" dir))
-         (buffer (current-buffer)))
-    (if zw-dired-sidebar-mode
-        (with-current-buffer buffer
-          (dired-hide-details-mode t)
-          (rename-buffer name)
-          (setq-local mode-line-format
-                      (list "%e" " "
-                            `(:eval (propertize ,(concat current-dir zw/modeline-separator)
-                                                'face (zw/modeline-set-face 'zw/modeline-major-mode-active
-                                                                            'zw/modeline-default-inactive)))
-                            '(:eval (zw/modeline-remote))
-                            '(:eval (zw/modeline-middle-space (zw/dired-sidebar-modeline-major-mode)))))
-          (set-window-dedicated-p (get-buffer-window buffer) t))
-      (with-current-buffer buffer
-        (dired-hide-details-mode 0)
-        (rename-buffer dir)
-        (setq-local mode-line-format (default-value 'mode-line-format))
-        (set-window-dedicated-p (get-buffer-window buffer) nil)))))
+    (,(kbd "C-x 1") . zw/dired-sidebar-maximize)))
 
 ;; * Openwith
 (defvar open-app-command (pcase system-type
