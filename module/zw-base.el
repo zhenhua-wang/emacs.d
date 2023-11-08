a30f838a9ac19c6da2a8bf9257afac9c0871afbb
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2023-11-08 01:50:36 -0600

    update

--- module/zw-base.el
+++ module/zw-base.el
@@ -69,7 +69,8 @@ The order of values may be different."
 
 ;; * Config
 (setq-default default-directory (concat (getenv "HOME") "/")
-              confirm-kill-emacs 'yes-or-no-p)
+              confirm-kill-emacs 'yes-or-no-p
+              large-file-warning-threshold nil)
 
 ;; default coding
 (set-default-coding-systems 'utf-8)
