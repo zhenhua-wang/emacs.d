fba9418eb92b67955636b9cb183998bd71304ae2
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-01-21 22:16:57 -0600

    update

--- module/zw-appearance.el
+++ module/zw-appearance.el
@@ -142,7 +142,8 @@
                             (number-sequence 0 9)))
     (define-key centaur-tabs-mode-map (car key-func) (cdr key-func)))
   ;; disable centuar-tabs in non-files
-  (add-hook 'magit-mode-hook 'centaur-tabs-local-mode)
+  (dolist (mode '(magit-mode-hook))
+    (add-hook mode 'centaur-tabs-local-mode))
   (add-hook 'window-configuration-change-hook
             (lambda ()
               (unless buffer-file-name
