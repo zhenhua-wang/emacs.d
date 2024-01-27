fc70964adf337ff37f54a79d73c493f8d99902e2
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-01-27 00:41:36 -0600

    use lsp-mode locally and eglot remotely

--- module/zw-ide.el
+++ module/zw-ide.el
@@ -1,17 +1,85 @@
 ;; -*- lexical-binding: t -*-
 
 ;; * LSP
+;; ** lsp-mode
+(setenv "LSP_USE_PLISTS" "true")
+(setq-default read-process-output-max (* 1024 1024)
+              lsp-use-plists t)
+(use-package lsp-mode
+  :commands (lsp-deferred)
+  :hook
+  (python-mode . zw/lsp-locally)
+  (ess-r-mode . zw/lsp-locally)
+  ((c++-mode c-mode) . zw/lsp-locally)
+  :init
+  (setq lsp-auto-guess-root t
+        lsp-keep-workspace-alive nil
+        lsp-keymap-prefix nil
+        ;; modeline
+        lsp-modeline-code-actions-enable nil
+        lsp-modeline-diagnostics-enable nil
+        lsp-modeline-workspace-status-enable nil
+        lsp-headerline-breadcrumb-enable nil
+        ;; doc
+        lsp-eldoc-enable-hover nil
+        lsp-signature-auto-activate nil
+        lsp-signature-render-documentation nil
+        ;; others
+        lsp-enable-folding nil
+        lsp-completion-provider :none
+        lsp-enable-indentation nil
+        lsp-enable-on-type-formatting nil
+        lsp-diagnostics-disabled-modes '(markdown-mode gfm-mode)
+        lsp-enable-file-watchers nil
+        lsp-enable-text-document-color nil)
+  :config
+  (defun zw/lsp-locally ()
+    (unless (file-remote-p default-directory)
+      (lsp-deferred))))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :bind ((:map lsp-ui-mode-map
+               ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+               ([remap xref-find-references] . lsp-ui-peek-find-references))
+         (:map lsp-ui-doc-mode-map
+               ("s-d" . lsp-ui-doc-toggle)))
+  :init (setq lsp-ui-imenu-enable nil
+              lsp-ui-sideline-enable nil
+              lsp-ui-peek-always-show t
+              lsp-ui-doc-position 'at-point
+              lsp-ui-doc-max-width 120
+              lsp-ui-doc-show-with-mouse nil)
+  ;; use posframe to show peek
+  (defun lsp-ui-peek--peek-display (src1 src2)
+    (-let* ((win-width (frame-width))
+            (lsp-ui-peek-list-width (/ (frame-width) 2))
+            (string (-some--> (-zip-fill "" src1 src2)
+                              (--map (lsp-ui-peek--adjust win-width it) it)
+                              (-map-indexed 'lsp-ui-peek--make-line it)
+                              (-concat it (lsp-ui-peek--make-footer)))))
+           (setq lsp-ui-peek--buffer (get-buffer-create " *lsp-peek--buffer*"))
+           (posframe-show lsp-ui-peek--buffer
+                          :string (mapconcat 'identity string "")
+                          :min-width (frame-width)
+                          :poshandler #'posframe-poshandler-frame-center)))
+  (defun lsp-ui-peek--peek-destroy ()
+    (when (bufferp lsp-ui-peek--buffer)
+      (posframe-delete lsp-ui-peek--buffer))
+    (setq lsp-ui-peek--buffer nil
+          lsp-ui-peek--last-xref nil)
+    (set-window-start (get-buffer-window) lsp-ui-peek--win-start))
+  (advice-add #'lsp-ui-peek--peek-new :override #'lsp-ui-peek--peek-display)
+  (advice-add #'lsp-ui-peek--peek-hide :override #'lsp-ui-peek--peek-destroy))
+
+;; ** eglot
 (use-package eglot
-  :hook ((python-mode . eglot-ensure)
-         (ess-r-mode . eglot-ensure)
-         ((c++-mode c-mode) . eglot-ensure))
   :bind (:map eglot-mode-map
               ([remap display-local-help] . nil)
               ("s-h" . display-local-help)
               ("s-d" . eldoc))
   :config
-  (setq read-process-output-max (* 1024 1024)
-        eglot-autoshutdown t
+  (setq eglot-autoshutdown t
         eglot-send-changes-idle-time 0.5)
   ;; patch for polymode
   (with-eval-after-load "polymode"
