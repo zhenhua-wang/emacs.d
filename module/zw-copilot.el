7aa42fd8b85c61f28407f6a8fdaeaba6591e134c
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-02-07 10:40:18 -0600

    add copilot

---
+++ module/zw-copilot.el
@@ -0,0 +1,19 @@
+;; -*- lexical-binding: t -*-
+
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
+  :hook ((prog-mode . copilot-mode))
+  :bind (("M-<iso-lefttab>" . copilot-complete)
+         :map copilot-completion-map
+         ("C-g" . 'copilot-clear-overlay)
+         ("M-p" . 'copilot-previous-completion)
+         ("M-n" . 'copilot-next-completion)
+         ("<tab>" . 'copilot-accept-completion)
+         ("<right>" . 'copilot-accept-completion-by-word)
+         ("<down>" . 'copilot-accept-completion-by-line))
+  :config
+  (add-to-list 'copilot-disable-predicates #'company--active-p)
+  (add-to-list 'copilot-disable-display-predicates #'company--active-p)
+  (setq copilot-indent-offset-warning-disable t))
+
+(provide 'zw-copilot)
