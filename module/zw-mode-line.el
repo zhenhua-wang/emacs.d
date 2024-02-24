b7f2b11502963f156692e97c6fe2188ff58fddc9
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-02-24 03:07:28 -0600

    update

--- module/zw-mode-line.el
+++ module/zw-mode-line.el
@@ -342,7 +342,7 @@
               zw/modeline-separator))))
 
 ;; ** LSP
-(defvar zw/modeline-lsp-icon "⚡")
+(defvar zw/modeline-lsp-icon (nerd-icons-octicon "nf-oct-rocket" :height 1 :v-adjust 0.05))
 
 (defun zw/modeline-lsp ()
   (when (and (featurep 'lsp-mode) lsp-mode)
