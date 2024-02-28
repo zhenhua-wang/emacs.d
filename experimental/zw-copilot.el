b3ef6dafb48bf606f0400e6cbc03589c4b00536e
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-02-28 01:59:00 -0600

    update

--- experimental/zw-copilot.el
+++ experimental/zw-copilot.el
@@ -2,7 +2,7 @@
 
 (use-package copilot
   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
-  :hook ((prog-mode . copilot-mode))
+  :hook ((prog-mode . zw/activate-copilot))
   :bind (("C-<tab>" . copilot-complete)
          :map copilot-completion-map
          ("C-g" . 'copilot-clear-overlay)
@@ -15,6 +15,10 @@
   (setq copilot-indent-offset-warning-disable t)
   (add-to-list 'copilot-disable-predicates #'company--active-p)
   (add-to-list 'copilot-disable-display-predicates #'company--active-p)
-  (advice-add 'company-posframe-show :before 'copilot-clear-overlay))
+  (advice-add 'company-posframe-show :before 'copilot-clear-overlay)
+  (defun zw/activate-copilot ()
+    (if (> (buffer-size) copilot-max-char)
+        (message "Buffer size exceeds copilot max char limit. Copilot will not be activated.")
+      (copilot-mode 1))))
 
 (provide 'zw-copilot)
