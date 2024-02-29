4fd0cb349b96c7698b68df833c475513279b39ad
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-02-28 21:39:48 -0600

    update

--- experimental/zw-dashboard.el
+++ experimental/zw-dashboard.el
@@ -3,10 +3,13 @@
 (use-package dashboard
   :config
   (dashboard-setup-startup-hook)
-  (setq dashboard-startup-banner 'logo
+  (setq dashboard-startup-banner 2
         dashboard-center-content t
         dashboard-vertically-center-content t
-        dashboard-set-navigator t)
+        dashboard-set-navigator t
+        dashboard-display-icons-p t
+        dashboard-icon-type 'nerd-icons
+        dashboard-set-file-icons t)
   (setq dashboard-items '((recents  . 5)))
   (setq dashboard-navigator-buttons
         `(((,(nerd-icons-codicon "nf-cod-github" :height 1.1 :v-adjust 0.0)
