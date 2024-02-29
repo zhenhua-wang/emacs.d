2f569a9d98ae7b33f3af41d07d9bbb96a81e8c2f
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-02-28 21:54:45 -0600

    update

--- experimental/zw-dashboard.el
+++ experimental/zw-dashboard.el
@@ -12,7 +12,7 @@
         dashboard-set-file-icons t)
   (setq dashboard-items '((recents  . 5)))
   (setq dashboard-navigator-buttons
-        `(((,(nerd-icons-codicon "nf-cod-github" :height 1.1)
+        `(((,(nerd-icons-mdicon "nf-md-github" :height 1.1)
             "Homepage"
             "Browse homepage"
             (lambda (&rest _) (browse-url "https://github.com/zhenhua-wang/emacs.d")))
