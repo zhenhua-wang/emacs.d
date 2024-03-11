5f182cac7d2f05e7419ecf98fe3c7d40fbf4f916
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-03-10 23:46:30 -0500

    update

--- module/zw-tools.el
+++ module/zw-tools.el
@@ -401,5 +401,43 @@
 (use-package psearch
   :straight (:host github :repo "twlz0ne/psearch.el"))
 
+;; * Pyim
+(use-package pyim
+  :commands (pyim-cregexp-build)
+  :bind ((:map pyim-mode-map
+               ("," . pyim-previous-page)
+               ("." . pyim-next-page)
+               ("[" . pyim-previous-page)
+               ("]" . pyim-next-page)
+               ("<left>" . pyim-backward-point)
+               ("<right>" . pyim-forward-point)
+               ("C-\\" . pyim/toggle-input-method)
+               ("s-\\" . pyim/toggle-input-method)))
+  :init
+  (setq default-input-method "pyim"
+        pyim-page-tooltip 'posframe
+        pyim-default-scheme 'quanpin
+        pyim-page-style 'two-line
+        pyim-page-length 9
+        pyim-cloudim 'google)
+  ;; vertico search pinyin
+  (defun pyim-orderless-regexp (orig-func component)
+    (let ((result (funcall orig-func component)))
+      (pyim-cregexp-build result)))
+  (advice-add 'orderless-regexp :around #'pyim-orderless-regexp)
+  :config
+  ;; toggle input entered pinyin
+  (defun pyim/toggle-input-method ()
+    (interactive)
+    (let ((word (pyim-entered-get)))
+      (pyim-quit-clear)
+      (funcall-interactively #'toggle-input-method)
+      (insert word))))
+
+(use-package pyim-basedict
+  :after pyim
+  :config
+  (pyim-basedict-enable))
+
 ;; * Provide
 (provide 'zw-tools)
