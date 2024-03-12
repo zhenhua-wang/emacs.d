9a13419bfb724243b8f3c67c315cf2c4b27cdbed
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-03-11 21:55:19 -0500

    update

--- module/zw-tools.el
+++ module/zw-tools.el
@@ -402,5 +402,25 @@
 (use-package psearch
   :straight (:host github :repo "twlz0ne/psearch.el"))
 
+;; * Rime
+(use-package rime
+  :init
+  (setq default-input-method "rime")
+  :config
+  (setq rime-show-preedit t
+        rime-user-data-dir "~/.cache/emacs/rime/"
+        rime-show-candidate 'posframe
+        rime-posframe-properties (list :internal-border-width 2))
+  ;; init user config
+  (let* ((dir rime-user-data-dir)
+         (config (expand-file-name "default.custom.yaml" dir)))
+    (unless (file-directory-p dir)
+      (make-directory dir t))
+    (unless (file-exists-p config)
+      (make-symbolic-link (expand-file-name "default.custom.yaml"
+                                            "~/.local/share/fcitx5/rime/")
+                          config t)))
+  (add-hook 'kill-emacs-hook #'rime-lib-finalize))
+
 ;; * Provide
 (provide 'zw-tools)
