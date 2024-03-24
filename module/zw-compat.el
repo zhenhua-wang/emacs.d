477eae6ce25cd65a5b2fefe36b22f234fcac36c6
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-03-24 15:08:50 -0400

    update

--- module/zw-compat.el
+++ module/zw-compat.el
@@ -2,20 +2,7 @@
 
 (unless (fboundp 'string-pixel-width)
   (defun string-pixel-width (string)
-    (if (zerop (length string))
-        0
-      ;; Keeping a work buffer around is more efficient than creating a
-      ;; new temporary buffer.
-      (with-current-buffer (get-buffer-create " *string-pixel-width*")
-        ;; If `display-line-numbers' is enabled in internal buffers
-        ;; (e.g. globally), it breaks width calculation (bug#59311)
-        (setq-local display-line-numbers nil)
-        (delete-region (point-min) (point-max))
-        ;; Disable line-prefix and wrap-prefix, for the same reason.
-        (setq line-prefix nil
-	      wrap-prefix nil)
-        (insert (propertize string 'line-prefix nil 'wrap-prefix nil))
-        (car (buffer-text-pixel-size nil nil t))))))
+    (string-width string)))
 
 (unless (fboundp 'mode-line-window-selected-p)
   (defun mode-line-window-selected-p ()
