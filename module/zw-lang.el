91b034c719df4cf84276db3eb3f877eb047b19f7
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-03-25 23:03:14 -0400

    update

--- module/zw-lang.el
+++ module/zw-lang.el
@@ -113,12 +113,12 @@
     (cond ((executable-find "ipython")
            (setq python-shell-interpreter "ipython"
                  python-shell-interpreter-args
-                 "-i --simple-prompt --InteractiveShell.display_page=True"))
+                 "-i --simple-prompt --InteractiveShell.display_page=True")
+           (add-to-list 'python-shell-completion-native-disabled-interpreters
+                        "ipython"))
           (t
            (setq python-shell-interpreter "python3"
                  python-shell-interpreter-args "-i")))
-    (add-to-list 'python-shell-completion-native-disabled-interpreters
-                 python-shell-interpreter)
     (when (and (featurep 'lsp-mode) lsp-mode)
       (lsp-restart-workspace))
     (when (and (featurep 'eglot) eglot--managed-mode)
