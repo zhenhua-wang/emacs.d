74c067987612c1f59c7f249af745e83de3b24d74
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-05-08 15:34:21 -0500

    remove dash-to-panel

--- resources/scripts/zw-gnome-config.sh
+++ resources/scripts/zw-gnome-config.sh
@@ -4,6 +4,7 @@ gsettings set org.gnome.desktop.interface enable-hot-corners false
 gsettings set org.gnome.desktop.privacy remember-recent-files false
 gsettings set org.gnome.system.location enabled true
 gsettings set org.gnome.desktop.datetime automatic-timezone true
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'firefox.desktop', 'org.gnome.DiskUtility.desktop', 'io.github.celluloid_player.Celluloid.desktop', 'org.gnome.baobab.desktop', 'com.obsproject.Studio.desktop', 'app.drey.Warp.desktop', 'io.gitlab.adhami3310.Impression.desktop', 'com.github.finefindus.eyedropper.desktop', 'com.vixalien.sticky.desktop', 'com.github.johnfactotum.Foliate.desktop', 'de.haeckerfelix.Fragments.desktop']"
 # window
 gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Super>"
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
@@ -22,21 +23,6 @@ gnome-extensions enable Vitals@CoreCoding.com
 gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
 gnome-extensions enable hide-universal-access@akiirui.github.io
 gnome-extensions enable caffeine@patapon.info
-# dash to panel
-gsettings set org.gnome.shell.extensions.dash-to-panel panel-positions '{"0":"TOP"}'
-gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-unfocused 'DASHES'
-gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-focused 'DASHES'
-gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-bg true
-gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-opacity true
-gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-dynamic-opacity true
-gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-gradient true
-gsettings set org.gnome.shell.extensions.dash-to-panel show-favorites false
-gsettings set org.gnome.shell.extensions.dash-to-panel animate-appicon-hover true
-gsettings set org.gnome.shell.extensions.dash-to-panel overview-click-to-exit true
-gsettings set org.gnome.shell.extensions.dash-to-panel hide-overview-on-startup true
-gsettings set org.gnome.shell.extensions.dash-to-panel stockgs-keep-dash true
-gsettings set org.gnome.shell.extensions.dash-to-panel panel-element-positions '{"0":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":true,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"centerMonitor"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":false,"position":"stackedBR"}]}'
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'firefox.desktop', 'org.gnome.DiskUtility.desktop', 'io.github.celluloid_player.Celluloid.desktop', 'org.gnome.baobab.desktop', 'com.obsproject.Studio.desktop', 'app.drey.Warp.desktop', 'io.gitlab.adhami3310.Impression.desktop', 'com.github.finefindus.eyedropper.desktop', 'com.vixalien.sticky.desktop', 'com.github.johnfactotum.Foliate.desktop', 'de.haeckerfelix.Fragments.desktop']"
 # vitals
 gsettings set org.gnome.shell.extensions.vitals hot-sensors "['__network-rx_max__', '__temperature_max__']"
 gsettings set org.gnome.shell.extensions.vitals hide-icons true
