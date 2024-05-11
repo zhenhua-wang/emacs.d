7b96f740f7890076e4be17ecb19a54bcb8f36d07
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-05-11 02:39:27 -0500

    update

--- resources/scripts/zw-gnome-bootstrap.sh
+++ resources/scripts/zw-gnome-bootstrap.sh
@@ -35,8 +35,9 @@ sudo systemctl enable bluetooth.service
 yay -S pandoc texlive-core texlive-latexextra texlive-fontsrecommended texlive-binextra texlive-mathscience texlive-plaingeneric texlive-bibtexextra miniconda3
 
 # zsh
-yay -S zsh && chsh -s $(which zsh)
+yay -S zsh zsh-syntax-highlighting && chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e 'source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
 echo -e '[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"' >> ~/.zshrc
 
 # config git
