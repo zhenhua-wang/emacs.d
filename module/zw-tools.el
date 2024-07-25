7bb608cd6d6911d1452f95dbb025b8bfcf1765c3
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-07-25 00:38:38 -0500

    update

--- module/zw-tools.el
+++ module/zw-tools.el
@@ -396,10 +396,7 @@
          (list (openwith-make-extension-regexp
                 '("doc" "docx" "xls" "xlsx" "ppt" "pptx" "odt" "ods" "odg" "odp"
                   "mpg" "mpeg" "mp3" "mp4" "avi" "wmv" "wav" "mov" "flv" "ogm"
-                  "webm" "ogg" "mkv"
-                  "xbm" "pbm" "pgm" "ppm" "pnm"
-                  "png" "gif" "bmp" "tif" "jpeg" "jpg"
-                  "pdf" "ps" "ps.gz" "dvi"))
+                  "webm" "ogg" "mkv"))
                open-app-command
                '(file)))))
 
