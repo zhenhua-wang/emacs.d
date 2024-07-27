f536c8b0e1e03a57f5f1f8b173a14eb8ba8d4210
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-07-27 03:52:05 -0500

    update

--- resources/scripts/emacs-pager.sh
+++ resources/scripts/emacs-pager.sh
@@ -13,5 +13,5 @@ cat - \
    | sed 's/\x1b\][0-9;:]*[AC]\x1b\\//g' \
     >> $t
 
-sh -c 'emacs "$@" < /dev/tty' emacs -Q -nw --eval="(progn (menu-bar-mode -1) (load-theme (quote modus-vivendi) t) (global-set-key (kbd \"q\") (quote kill-emacs)) (global-set-key (kbd \"<ESC>\") (quote kill-emacs)) (define-key isearch-mode-map (kbd \"<ESC>\") (quote kill-emacs)) (add-hook (quote find-file-hook) (lambda () (read-only-mode 1) (setq-local mode-line-format nil) (call-interactively (quote isearch-forward)))))" "$t"
+sh -c 'emacs "$@" < /dev/tty' emacs -Q -nw --eval="(progn (setq user-emacs-directory (expand-file-name \"~/.cache/emacs/\")) (push \"~/.emacs.d/module\" load-path) (menu-bar-mode -1) (load-theme (quote modus-vivendi) t) (require 'zw-compat) (require 'zw-package) (require 'zw-startup) (global-set-key (kbd \"q\") (quote kill-emacs)) (global-set-key (kbd \"<escape>\") (quote kill-emacs)) (define-key isearch-mode-map (kbd \"<escape>\") (quote kill-emacs)) (global-set-key (kbd \"s-f\") (quote isearch-forward)) (define-key isearch-mode-map (kbd \"s-f\") (quote isearch-repeat-forward)) (setq isearch-wrap-pause 'no) (add-hook (quote find-file-hook) (lambda () (read-only-mode 1) (setq-local mode-line-format nil) (call-interactively (quote isearch-forward)))))" "$t"
 rm -f $t
