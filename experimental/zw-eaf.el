b049a0e0ed3d1fed1d929b78d74ab948c3382926
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-07-28 16:18:07 -0500

    update

--- experimental/zw-eaf.el
+++ experimental/zw-eaf.el
@@ -11,13 +11,14 @@
   :init
   (setq zw/eaf-bin "~/.conda/envs/eaf/bin"
         eaf-python-command (expand-file-name "python3" zw/eaf-bin))
-  (defun eaf-install ())
-  (defun eaf-install-and-update ())
   (defun zw/eaf-install ()
     (interactive)
     (let ((zw/eaf-image-viewer-path (expand-file-name
                                      "straight/build/eaf-image-viewer" user-emacs-directory)))
-      (async-shell-command (format "/opt/miniconda3/bin/conda create -n eaf python=3.11 && source /opt/miniconda3/bin/activate eaf && conda install conda-forge::wmctrl conda-forge::nodejs conda-forge::qt6-multimedia && pip install packaging PyQt6-WebEngine PyQt6 PyQt6-sip setuptools sexpdata epc pymupdf && npm install %s --prefix %s" zw/eaf-image-viewer-path zw/eaf-image-viewer-path)))))
+      (async-shell-command (format "/opt/miniconda3/bin/conda create -n eaf python=3.11 && source /opt/miniconda3/bin/activate eaf && conda install conda-forge::wmctrl conda-forge::nodejs conda-forge::qt6-multimedia && pip install packaging PyQt6-WebEngine PyQt6 PyQt6-sip setuptools sexpdata epc pymupdf && npm install %s --prefix %s" zw/eaf-image-viewer-path zw/eaf-image-viewer-path))))
+  :config
+  (advice-add 'eaf-install :override (lambda (&rest _)))
+  (advice-add 'eaf-install-and-update :override (lambda (&rest _))))
 
 (use-package eaf-pdf-viewer
   :straight (:type git :host github :repo "emacs-eaf/eaf-pdf-viewer"
