c19b8a6b55b621eb892969ec531e7c03448d7f67
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-07-28 03:27:08 -0500

    update

--- resources/pager/init.el
+++ resources/pager/init.el
@@ -66,13 +66,18 @@
       search-nonincremental-instead nil
       search-whitespace-regexp ".*?"
       lazy-count-prefix-format "%s/%s "
-      visible-bell t)
+      visible-bell t
+      hscroll-margin 1
+      hscroll-step 1)
 (add-hook 'find-file-hook (lambda ()
                             ;; pager
                             (require 'ansi-color)
                             (ansi-color-apply-on-region (point-min) (point-max))
                             (read-only-mode 1)
                             (hl-line-mode 1)
+                            (auto-save-mode 0)
+                            (toggle-truncate-lines 1)
+                            (set-display-table-slot standard-display-table 'truncation 32)
                             (global-clipetty-mode 1)
                             (kkp-status)
                             (kkp-enable-in-terminal)
