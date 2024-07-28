98f0adaf9589ef67e5633dced5c92e7d44263b59
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-07-28 00:30:10 -0500

    update

--- resources/pager/emacs-pager.sh
+++ resources/pager/emacs-pager.sh
@@ -7,10 +7,11 @@ t=$(mktemp /tmp/emacs-pager.XXXXXX) || exit 1
 
 echo "Reading into emacs..."
 
-# Remove terminal escape sequences (color and move, as well as some funky starship stuff)
+# Remove terminal escape sequences (color and move, as well as some funky starship stuff) and empty lines
 cat - \
    | sed 's/\x1b\[[0-9;:]*[mGKH]//g' \
    | sed 's/\x1b\][0-9;:]*[AC]\x1b\\//g' \
+   | sed '/^$/d' \
     >> $t
 
 sh -c 'emacs "$@" < /dev/tty' emacs -nw --init-directory=~/.emacs.d/resources/pager "$t"
