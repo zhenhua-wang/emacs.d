48d51a480e4e96aa279774aeb5bd761b6ccb0388
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-08-01 14:01:02 -0500

    update

--- experimental/zw-eaf.el
+++ experimental/zw-eaf.el
@@ -19,7 +19,7 @@
     (let ((zw/eaf-image-viewer-path (expand-file-name
                                      "straight/build/eaf-image-viewer" user-emacs-directory)))
       (async-shell-command (format "/opt/miniconda3/bin/conda create -n eaf python=3.11 && source /opt/miniconda3/bin/activate eaf && conda install conda-forge::wmctrl conda-forge::nodejs conda-forge::qt6-multimedia && pip install packaging PyQt6-WebEngine PyQt6 PyQt6-sip setuptools sexpdata epc pymupdf && npm install %s --prefix %s" zw/eaf-image-viewer-path zw/eaf-image-viewer-path))))
-  (defun zw/eaf-install-app ()
+  (defun zw/eaf-compile-app ()
     (interactive)
     (let ((zw/eaf-image-viewer-path (expand-file-name
                                      "straight/build/eaf-image-viewer" user-emacs-directory)))
