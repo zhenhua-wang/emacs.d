edf67ef38d5f6d24c76a36e0c9cb4295cd2323c7
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-08-01 18:41:38 -0500

    update

--- resources/exwm/start-exwm.sh
+++ resources/exwm/start-exwm.sh
@@ -28,10 +28,10 @@ export EDITOR="$VISUAL"
 export _JAVA_AWT_WM_NONREPARENTING=1
 
 # scale DPI
-export EXWM_DPI=$(xrdb -query | grep dpi | grep -o '[0-9]\+')
-export EXWM_DPI_SCALE=$((EXWM_DPI/96))
-export GDK_SCALE=$((EXWM_DPI_SCALE))
-export GDK_DPI_SCALE=$(awk -v var="$EXWM_DPI_SCALE" "BEGIN {print 1/var}")
+# export EXWM_DPI=$(xrdb -query | grep dpi | grep -o '[0-9]\+')
+# export EXWM_DPI_SCALE=$((EXWM_DPI/96))
+# export GDK_SCALE=$((EXWM_DPI_SCALE))
+# export GDK_DPI_SCALE=$(awk -v var="$EXWM_DPI_SCALE" "BEGIN {print 1/var}")
 
 # Fire it up
 # exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.emacs.d/module/zw-exwm.el
