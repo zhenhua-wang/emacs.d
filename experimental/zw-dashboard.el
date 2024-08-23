1d220f3b69821bf5cc6a501e24b34278d0ba9d13
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-08-23 14:13:00 -0500

    update

--- experimental/zw-dashboard.el
+++ experimental/zw-dashboard.el
@@ -21,7 +21,8 @@
                                dashboard-insert-items
                                dashboard-insert-newline
                                dashboard-insert-init-info))
-  (dashboard-items '((recents . 5)
+  (dashboard-items '((agenda . 5)
+                     ;; (recents . 5)
                      (projects . 5)))
   (dashboard-navigator-buttons
    `(((,(nerd-icons-mdicon "nf-md-github" :height 1.1)
