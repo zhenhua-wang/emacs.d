ad46132f8f196afbe7ad329692b32277ae7e7f16
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-08-27 13:17:23 -0500

    update

--- resources/scripts/zw-gnome-bootstrap.sh
+++ resources/scripts/zw-gnome-bootstrap.sh
@@ -1,25 +1,28 @@
 cd ~
 # yay
-sudo pacman -S --needed git base-devel cmake inetutils
+sudo pacman -S --noconfirm --needed git base-devel cmake inetutils
 git clone https://aur.archlinux.org/yay.git
-cd ~/yay && makepkg -si && cd ~
+cd ~/yay && makepkg -si --noconfirm && cd ~
+alias yay='yay --noconfirm'
 
 # fonts
-yay -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji
+yay -S noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 # emacs
 mv ~/emacs.d ~/.emacs.d
-yay -S --noconfirm emacs-nativecomp emacs-lsp-booster aspell aspell-en
+yay -S emacs-nativecomp emacs-lsp-booster aspell aspell-en
 emacs -Q --batch --eval "(require 'org)" --eval '(setq user-emacs-directory (expand-file-name "~/.cache/emacs/"))' --eval '(defun zw/org-babel-tangle-linux (path) (if (eq system-type (intern "gnu/linux")) path "no"))' --eval '(defun zw/org-babel-tangle-not-exist (path) (if (file-exists-p path) "no" path))' --eval '(org-babel-tangle-file "~/.emacs.d/resources/OrgFiles/dotfiles.org")'
 
 # gnome core
-yay -S --noconfirm gdm gnome-shell gnome-control-center gnome-keyring gnome-tweaks networkmanager xdg-desktop-portal-gnome xdg-user-dirs gst-plugins-good power-profiles-daemon switcheroo-control flatpak malcontent iio-sensor-proxy networkmanager-openconnect gnome-remote-desktop gnome-firmware nautilus gnome-clocks gnome-calculator gnome-disk-utility gnome-system-monitor
+yay -S gdm gnome-shell gnome-control-center gnome-keyring gnome-tweaks networkmanager xdg-desktop-portal-gnome xdg-user-dirs gst-plugins-good power-profiles-daemon switcheroo-control flatpak malcontent iio-sensor-proxy networkmanager-openconnect gnome-remote-desktop gnome-firmware nautilus gnome-clocks gnome-calculator gnome-disk-utility gnome-system-monitor
 sudo systemctl enable gdm.service -f
 # gnome extra
-yay -S --noconfirm gnome-themes-extra gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-vitals ibus-rime rime-ice-git
+yay -S gnome-themes-extra gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-vitals ibus-rime rime-ice-git
+# xorg for compatibility
+yay -S xorg-server
 
 # xremap
-yay -S --noconfirm xremap-x11-bin
+yay -S xremap-x11-bin
 sudo gpasswd -a $USER input
 echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/99-input.rules
 ######## if not working on gnome wayland, try 'xremap-gnome-bin' ########
@@ -34,7 +37,7 @@ echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rule
 sudo systemctl enable bluetooth.service
 
 # zsh
-yay -S --noconfirm zsh zsh-syntax-highlighting zsh-autosuggestions && chsh -s $(which zsh)
+yay -S zsh zsh-syntax-highlighting zsh-autosuggestions && chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo -e 'source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
 echo -e 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
@@ -48,10 +51,10 @@ git config --global user.email "wang_zhenhua1996@icloud.com"
 git config --global credential.helper store
 
 # essential desktop
-yay -S --noconfirm kitty fzf htop fastfetch ripgrep mpv yt-dlp streamlink file-roller exfat-utils
+yay -S kitty fzf htop fastfetch ripgrep mpv yt-dlp streamlink file-roller exfat-utils
 
 # research
-yay -S --noconfirm pandoc-cli texlive-basic texlive-latexextra texlive-fontsrecommended texlive-binextra texlive-mathscience texlive-plaingeneric texlive-bibtexextra miniconda3
+yay -S pandoc-cli texlive-basic texlive-latexextra texlive-fontsrecommended texlive-binextra texlive-mathscience texlive-plaingeneric texlive-bibtexextra miniconda3
 
 # flatpak applications
 flatpak --assumeyes install org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal org.gnome.baobab org.gnome.Loupe org.gnome.Connections com.github.johnfactotum.Foliate de.haeckerfelix.Fragments io.gitlab.adhami3310.Impression app.drey.Warp com.github.finefindus.eyedropper com.obsproject.Studio org.gnome.Evince org.gimp.GIMP org.libreoffice.LibreOffice us.zoom.Zoom com.google.Chrome net.nokyan.Resources
@@ -59,9 +62,6 @@ sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --filesystem=$HOME/.icons
 # grant firefox permission to temp folder
 flatpak override --user org.mozilla.firefox --filesystem="/tmp"
-
-# xorg for compatibility
-yay -S --noconfirm xorg-server
 
 # config
 bash ~/.emacs.d/resources/scripts/zw-gnome-config.sh
