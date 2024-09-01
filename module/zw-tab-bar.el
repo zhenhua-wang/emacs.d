fcfde3fe9b0d82d8973d30684b575120bf141259
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-08-31 20:06:07 -0500

    update

--- module/zw-tab-bar.el
+++ module/zw-tab-bar.el
@@ -78,7 +78,7 @@
    (append (mapcar (lambda (x)
                      (vector x `(lambda () (interactive) (conda-env-activate ,x) t)))
                    (conda-env-candidates))
-           (list "---" "---")
+           (list "---")
            (list (vector "conda deactivate"
                          `(lambda () (interactive) (conda-env-deactivate) t))))))
 
