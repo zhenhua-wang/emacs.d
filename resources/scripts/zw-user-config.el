2583772113ecaed3bb7c73d9ddc82e0cc2051a52
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-10-24 10:11:11 -0500

    update

--- resources/scripts/zw-user-config.el
+++ resources/scripts/zw-user-config.el
@@ -6,8 +6,10 @@
 (add-hook 'zw/after-set-theme-hook 'zw/ui-padding-mode)
 (add-hook 'after-init-hook 'keycast-tab-bar-mode)
 
-(setq
- zw/modeline-remote-show-local nil
- zw/tab-line-show-debug nil
- ;; company-idle-delay 0
- )
+(setq zw/modeline-remote-show-local nil
+      zw/tab-line-show-debug nil)
+
+;; auto complete with preselection
+;; (setq company-idle-delay 0
+;;       company-selection-default nil
+;;       company-frontends '(company-pseudo-tooltip-frontend))
