9b272aae3a567490aca713f039416c22e1f5e3fe
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-10-31 18:03:03 -0500

    update

--- experimental/zw-tool-bar.el
+++ experimental/zw-tool-bar.el
@@ -14,7 +14,7 @@
 (tool-bar-add-item-from-menu 'save-buffer "save" nil :label "Save")
 (define-key-after (default-value 'tool-bar-map) [separator-1] menu-bar-separator)
 (tool-bar-add-item "back-arrow" 'undo-only 'undo-only :label "Undo")
-(tool-bar-add-item "redo" 'undo-redo 'undo-redo :label "Redo")
+(tool-bar-add-item "fwd-arrow" 'undo-redo 'undo-redo :label "Redo")
 (define-key-after (default-value 'tool-bar-map) [separator-2] menu-bar-separator)
 
 (provide 'zw-tool-bar)
