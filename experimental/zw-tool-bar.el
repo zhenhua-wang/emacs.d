4912dee8c41967215640e7c6e963e040168b26da
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-10-31 11:47:13 -0500

    update

--- experimental/zw-tool-bar.el
+++ experimental/zw-tool-bar.el
@@ -1,7 +1,20 @@
 ;; -*- lexical-binding: t -*-
 
+;; global
 (push '(tool-bar-lines . 1)   initial-frame-alist)
 (push '(tool-bar-position . left)   initial-frame-alist)
 (setq tool-bar-style 'image)
+(setq tool-bar-map (make-sparse-keymap))
+(setq tool-bar-separator-image-expression
+      (tool-bar--image-expression "separator"))
+
+;; items
+(tool-bar-add-item-from-menu 'find-file "new" nil :label "New File")
+(tool-bar-add-item-from-menu 'menu-find-file-existing "open" nil :label "Open")
+(tool-bar-add-item-from-menu 'save-buffer "save" nil :label "Save")
+(define-key-after (default-value 'tool-bar-map) [separator-1] menu-bar-separator)
+(tool-bar-add-item "undo" 'undo-only 'undo-only :label "Undo")
+(tool-bar-add-item "redo" 'undo-redo 'undo-redo :label "Redo")
+(define-key-after (default-value 'tool-bar-map) [separator-2] menu-bar-separator)
 
 (provide 'zw-tool-bar)
