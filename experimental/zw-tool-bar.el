15bbec21999946fa29a6973c6bb0ffdf3bcf0250
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-11-01 12:37:13 -0500

    update

--- experimental/zw-tool-bar.el
+++ experimental/zw-tool-bar.el
@@ -9,12 +9,14 @@
       (tool-bar--image-expression "separator"))
 
 ;; items
+(tool-bar-add-item "home" 'zw/open-user-config 'zw/open-user-config :label "Emacs")
+(define-key-after (default-value 'tool-bar-map) [separator-1] menu-bar-separator)
 (tool-bar-add-item-from-menu 'find-file "new" nil :label "New File")
 (tool-bar-add-item-from-menu 'menu-find-file-existing "open" nil :label "Open")
 (tool-bar-add-item-from-menu 'save-buffer "save" nil :label "Save")
-(define-key-after (default-value 'tool-bar-map) [separator-1] menu-bar-separator)
+(define-key-after (default-value 'tool-bar-map) [separator-2] menu-bar-separator)
 (tool-bar-add-item "back-arrow" 'undo-only 'undo-only :label "Undo")
 (tool-bar-add-item "fwd-arrow" 'undo-redo 'undo-redo :label "Redo")
-(define-key-after (default-value 'tool-bar-map) [separator-2] menu-bar-separator)
+(define-key-after (default-value 'tool-bar-map) [separator-3] menu-bar-separator)
 
 (provide 'zw-tool-bar)
