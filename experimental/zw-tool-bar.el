f601be5742f0c357b436c18e9d273d06cfbe60f5
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2024-11-04 21:39:19 -0600

    Add custom button on tool-bar

--- experimental/zw-tool-bar.el
+++ experimental/zw-tool-bar.el
@@ -3,12 +3,21 @@
 ;; global
 (push '(tool-bar-lines . 1)   initial-frame-alist)
 (push '(tool-bar-position . left)   initial-frame-alist)
+(add-to-list 'image-load-path "~/.emacs.d/resources/images/icons/")
 (setq tool-bar-style 'image)
 (setq tool-bar-map (make-sparse-keymap))
 (setq tool-bar-separator-image-expression
       (tool-bar--image-expression "separator"))
 
 ;; functions
+(defun zw/tool-bar-add-item (command image help-echo)
+  "Add button (24px) on tab-bar."
+  (define-key-after (default-value 'tool-bar-map) (vector command)
+    `(menu-item "Open user configuration file" ,command
+                :enable t
+                :help ,help-echo
+                :image ,image)))
+
 (defun zw/tool-bar-home ()
   "Switch to scratch buffer."
   (interactive)
@@ -27,5 +36,8 @@
 (tool-bar-add-item "back-arrow" 'undo-only 'undo-only :label "Undo")
 (tool-bar-add-item "fwd-arrow" 'undo-redo 'undo-redo :label "Redo")
 (define-key-after (default-value 'tool-bar-map) [separator-3] menu-bar-separator)
+(zw/tool-bar-add-item 'zw/open-user-config
+                      (find-image '((:type png :file "setting.png" :scale 0.5)))
+                      "Open user configuration file")
 
 (provide 'zw-tool-bar)
