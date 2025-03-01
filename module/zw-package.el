b4847734e87ebd828bfffa573774f40a404ace94
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2025-03-01 17:17:10 -0600

    update

--- module/zw-package.el
+++ module/zw-package.el
@@ -14,16 +14,6 @@
 (package-activate-all)
 (add-hook 'package-menu-mode-hook 'zw/visual-line-disable)
 
-;; HACK: DO NOT save `package-selected-packages' to `custom-file'
-;; @see https://github.com/jwiegley/use-package/issues/383#issuecomment-247801751
-(defun my-package--save-selected-packages (&optional value)
-  "Set `package-selected-packages' to VALUE but don't save to option `custom-file'."
-  (when value
-    (setq package-selected-packages value))
-  (unless after-init-time
-    (add-hook 'after-init-hook #'my-package--save-selected-packages)))
-(advice-add 'package--save-selected-packages :override #'my-package--save-selected-packages)
-
 (defun zw/package-upgrade-all ()
   "Upgrade all installed packages."
   (interactive)
