95914c928165094b70165b804d836fc71adcedf3
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2025-04-10 04:46:13 -0500

    update

--- experimental/zw-theme-auto.el
+++ experimental/zw-theme-auto.el
@@ -7,6 +7,11 @@
          (auto-dark-dark-mode . zw/theme-load-ui)
          (auto-dark-light-mode . zw/theme-load-ui))
   :config
-  (setq auto-dark-themes '((adwaita-dark) (doom-one-light))))
+  (setq auto-dark-themes '((adwaita-dark) (doom-one-light)))
+  :init
+  (defun advice/auto-dark--enable-themes (auto-dark--enable-themes &rest args)
+    (let ((custom-safe-themes t))
+      (apply auto-dark--enable-themes args)))
+  (advice-add 'auto-dark--enable-themes :around 'advice/auto-dark--enable-themes))
 
 (provide 'zw-theme-auto)
