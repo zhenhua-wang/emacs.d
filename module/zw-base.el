8c671ee26f1ac666f966dfdaeb8455cf582edfe5
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2025-05-22 19:48:28 -0500

    update

--- module/zw-base.el
+++ module/zw-base.el
@@ -361,7 +361,7 @@ The order of values may be different."
                       nil zw/frame-parameters)
       (write-region
        (format "(push '(width . %s) initial-frame-alist)\n(push '(height . %s) initial-frame-alist)"
-               (+ (frame-parameter nil 'width) 25)
+               (+ (frame-parameter nil 'width) 28)
                (+ (frame-parameter nil 'height) 10))
        nil zw/frame-parameters))))
 (add-hook 'kill-emacs-hook 'zw/save-frame-parameters)
