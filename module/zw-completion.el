920912f640d1e7c17d93c475ad927d44f595d6f0
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2025-05-24 18:46:15 -0500

    update

--- module/zw-completion.el
+++ module/zw-completion.el
@@ -60,7 +60,7 @@
           vertico-posframe-poshandler 'posframe-poshandler-frame-center
           vertico-posframe-parameters '((left-fringe . 8)
                                         (right-fringe . 8))
-          vertico-posframe-width (floor (/ (display-pixel-width) (frame-char-width) 1.5))))
+          vertico-posframe-width (floor (/ (frame-pixel-width) (frame-char-width) 1.5))))
   (defun zw/vertico-posframe-refresh ()
     "Refresh vertico-posframe."
     (interactive)
