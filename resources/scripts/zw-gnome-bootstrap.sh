84305c4eefc5fb5e4af15415e5af428414036762
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2025-08-21 13:26:07 -0500

    update

--- resources/scripts/zw-gnome-bootstrap.sh
+++ resources/scripts/zw-gnome-bootstrap.sh
@@ -25,7 +25,7 @@ yay -S gdm gnome-shell gnome-control-center gnome-keyring gnome-tweaks networkma
 sudo systemctl enable gdm.service -f
 sudo ln -s /usr/bin/gnome-session-quit /usr/bin/logout-gnome
 # gnome extra
-yay -S gnome-themes-extra gnome-shell-extension-caffeine gnome-shell-extension-vitals baobab ibus-rime rime-ice-git gnome-weather gnome-shell-extension-weather-oclock rygel gnome-user-share gnome-calendar gnome-characters gnome-connections gnome-font-viewer gnome-software loupe snapshot
+yay -S gnome-themes-extra gnome-shell-extension-caffeine gnome-shell-extension-vitals baobab ibus-rime rime-ice-git gnome-weather gnome-shell-extension-weather-oclock rygel gnome-user-share gnome-calendar gnome-characters gnome-connections gnome-font-viewer gnome-software snapshot loupe papers showtime
 # gnome experiment
 yay -S grilo-plugins gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd
 # xorg and eaf compatibility
@@ -72,7 +72,7 @@ yay -S kitty htop fastfetch ripgrep mpv yt-dlp streamlink file-roller exfat-util
 yay -S pandoc-cli texlive-basic texlive-latexextra texlive-fontsrecommended texlive-binextra texlive-mathscience texlive-plaingeneric texlive-bibtexextra miniconda3
 
 # flatpak applications
-flatpak --assumeyes install org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal com.github.johnfactotum.Foliate de.haeckerfelix.Fragments io.gitlab.adhami3310.Impression app.drey.Warp com.github.finefindus.eyedropper com.obsproject.Studio org.gimp.GIMP org.libreoffice.LibreOffice us.zoom.Zoom net.nokyan.Resources org.gnome.Solanum com.github.wwmm.easyeffects app.zen_browser.zen org.gnome.Papers
+flatpak --assumeyes install org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal com.github.johnfactotum.Foliate de.haeckerfelix.Fragments io.gitlab.adhami3310.Impression app.drey.Warp com.github.finefindus.eyedropper com.obsproject.Studio org.gimp.GIMP org.libreoffice.LibreOffice us.zoom.Zoom net.nokyan.Resources org.gnome.Solanum com.github.wwmm.easyeffects app.zen_browser.zen
 sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --filesystem=$HOME/.icons
 # grant firefox permission to temp folder
