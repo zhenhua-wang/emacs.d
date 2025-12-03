18f81377b4d1163a6d27048c3dba33f0c0b354b5
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2025-12-02 20:24:08 -0600

    Replace pandoc-cli with pandoc-bin

--- resources/scripts/zw-gnome-bootstrap.sh
+++ resources/scripts/zw-gnome-bootstrap.sh
@@ -69,7 +69,7 @@ git config --global credential.helper store
 yay -S kitty htop fastfetch ripgrep mpv yt-dlp streamlink file-roller exfat-utils fprintd
 
 # research
-yay -S pandoc-cli texlive-basic texlive-latexextra texlive-fontsrecommended texlive-binextra texlive-mathscience texlive-plaingeneric texlive-bibtexextra miniconda3
+yay -S texlive-basic texlive-latexextra texlive-fontsrecommended texlive-binextra texlive-mathscience texlive-plaingeneric texlive-bibtexextra pandoc-bin miniconda3
 
 # flatpak applications
 flatpak --assumeyes install org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal com.github.johnfactotum.Foliate de.haeckerfelix.Fragments io.gitlab.adhami3310.Impression app.drey.Warp com.github.finefindus.eyedropper com.obsproject.Studio org.gimp.GIMP org.libreoffice.LibreOffice us.zoom.Zoom net.nokyan.Resources org.gnome.Solanum com.github.wwmm.easyeffects app.zen_browser.zen
