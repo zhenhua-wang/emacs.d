45588a40e12404d6bab03bd0d33a5868b69a1bee
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2026-01-09 16:57:17 -0500

    update

--- experimental/zw-kitty-image.el
+++ experimental/zw-kitty-image.el
@@ -1,17 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
-(defun kitty-clear-images ()
-  (interactive)
-  "Clear all kitty graphics from the terminal."
-  (kitty-image--write-tty "\e_Ga=d\e\\"))
-
 (defun kitty-image--write-tty (data)
   "Write DATA to /dev/tty directly."
   (let ((coding-system-for-write 'binary)
         (write-region-inhibit-fsync t))
     (write-region data nil "/dev/tty" t 0)))
 
-(defun kitty-image-display (file)
+(defun kitty-display-image (file)
   "Test direct image display in Kitty terminal."
   (interactive
    (list (read-file-name "Image: " default-directory nil t)))
@@ -21,8 +16,12 @@
                  (buffer-string)))
          (b64 (base64-encode-string data t))
          (test1 (format "\x1b_Ga=T,f=100,z=-1,t=d;%s\x1b\\" b64)))
-    (message "Test 1: Direct display")
     (kitty-image--write-tty test1)
     (sit-for 2)))
+
+(defun kitty-clear-image ()
+  (interactive)
+  "Clear all kitty graphics from the terminal."
+  (kitty-image--write-tty "\e_Ga=d\e\\"))
 
 (provide 'zw-kitty-image)
