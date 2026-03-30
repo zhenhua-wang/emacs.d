51d424888a87058e9d1957369359f552d549e237
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2026-03-30 10:27:46 -0500

    Add patch for 3fg drag for libinput

---
+++ resources/scripts/zw-patch-libinput.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting clean build for libinput..."
+if [ -d "builddir/" ]; then
+    echo "Removing existing builddir/..."
+    rm -rf builddir/
+fi
+
+echo "Applying patch..."
+patch -Np1 -i ~/.emacs.d/resources/scripts/libinput-enbale-3fg-drag.patch
+
+echo "Configuring with Meson..."
+meson setup --prefix=/usr builddir/
+
+echo "Compiling with Ninja..."
+ninja -C builddir/
+
+echo "Installing to system..."
+sudo ninja -C builddir/ install
+
+echo "Success! libinput with 3-finger drag has been installed."
