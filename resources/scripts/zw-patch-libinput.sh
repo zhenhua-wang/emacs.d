fde49ea5c2f88c93f7c2dd6473c784312128ce2c
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2026-04-07 11:55:32 -0500

    update

--- resources/scripts/zw-patch-libinput.sh
+++ resources/scripts/zw-patch-libinput.sh
@@ -8,6 +8,10 @@ if [ -d "builddir/" ]; then
     rm -rf builddir/
 fi
 
+echo "Pull latest version..."
+git reset --hard
+git pull
+
 echo "Applying patch..."
 patch -Np1 -i ~/.emacs.d/resources/scripts/libinput-enbale-3fg-drag.patch
 
